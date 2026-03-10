74beb315b85d467197b19c5d0a823e923f6ef544
Satyam <147689858+tiwari011@users.noreply.github.com>
2026-03-10 17:30:23 +0530

    Update firebase.js

--- shaadibio/src/firebase.js
+++ shaadibio/src/firebase.js
@@ -3,15 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0o5p6CXqKGGuT9mnvf4Ng5VZ8AhIc-m4",
-  authDomain: "project1-2b0ac.firebaseapp.com",
-  projectId: "project1-2b0ac",
-  storageBucket: "project1-2b0ac.firebasestorage.app",
-  messagingSenderId: "424719661288",
-  appId: "1:424719661288:web:0f282660378c108cc7315d",
-  measurementId: "G-JC8HYSDH6Y"
+ const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const analytics = getAnalytics(app);
